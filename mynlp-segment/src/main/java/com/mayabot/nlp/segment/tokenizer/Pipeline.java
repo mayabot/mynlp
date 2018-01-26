5aff65dbbff04fc8739d56935eb094472e3de9fd
jimichan <jimichan@mayabot.com>
2018-01-26 14:34:06 +0800

    代码格式化。 分词器包结构和开放类和接口定格
    Former-commit-id: 48c627e65358f00340f3aaf8d8915c3045418725 [formerly fa936e230ea2c67723e3c21b1f3619b70764b548]
    Former-commit-id: 929a82d46d3d40abc1304bd96fdd195cf336fef6

---
+++ mynlp-segment/src/main/java/com/mayabot/nlp/segment/tokenizer/Pipeline.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 mayabot.com authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mayabot.nlp.segment.tokenizer;
+
+import com.mayabot.nlp.segment.WordpathProcessor;
+import com.mayabot.nlp.segment.wordnet.Wordpath;
+
+import java.util.List;
+
+public class Pipeline {
+
+    private WordpathProcessor[] wordpathProcessors;
+
+
+    public Pipeline(List<WordpathProcessor> wordPathProcessors) {
+        this.wordpathProcessors = wordPathProcessors.toArray(new WordpathProcessor[0]);
+    }
+
+    public Wordpath process(Wordpath wordPath){
+        for (WordpathProcessor processor : wordpathProcessors) {
+            wordPath = processor.process(wordPath);
+        }
+        return wordPath;
+    }
+
+
+
+}
