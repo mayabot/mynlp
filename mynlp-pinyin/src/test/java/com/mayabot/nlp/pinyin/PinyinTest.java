a593973223376c313acac1d51bc4ff84a2b730fc
jimichan <jimichan@mayabot.com>
2018-01-29 14:04:44 +0800

    增加拼音功能

---
+++ mynlp-pinyin/src/test/java/com/mayabot/nlp/pinyin/PinyinTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 mayabot.com authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mayabot.nlp.pinyin;
+
+import com.mayabot.nlp.MyNlps;
+import com.mayabot.nlp.pinyin.model.Pinyin;
+import org.junit.Test;
+
+public class PinyinTest {
+
+    @Test
+    public void test() {
+        PinyinDictionary instance = MyNlps.getInjector().getInstance(PinyinDictionary.class);
+        Text2PinyinService text2PinyinService = MyNlps.getInjector().getInstance(Text2PinyinService.class);
+
+        PinyinResult xx = text2PinyinService.text2Pinyin("123aed,.你好朝朝暮暮");
+//        xx.ignorePunctuation = false;
+        System.out.println(xx.asString());
+        System.out.println(xx.asHeadString());
+
+    }
+
+
+}
