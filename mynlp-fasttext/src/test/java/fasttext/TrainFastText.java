cf9789c73e0bfbb0e2215dbbbfa4929f28e0ddc1
jimichan <jimichan@mayabot.com>
2018-03-14 15:59:00 +0800

    update
    Former-commit-id: 5ba7be84b86b91eca459a94b89191fb73656f23f [formerly d0970c7bdde5e449d55476b213c63ea98ccc0be8]
    Former-commit-id: 7d0a5111759b98bf4bf9e03acdbab6d5d8e1e34f

--- mynlp-fasttext/src/test/java/fasttext/TrainFastText.java
+++ mynlp-fasttext/src/test/java/fasttext/TrainFastText.java
@@ -23,6 +23,19 @@ public class TrainFastText {
         result.saveVectors(new File("data/out/model.vec"));
     }
 
+    public static void main(String[] args2) throws Exception{
+        File trFile = new File("data/train.txt");
+        Args args = new Args();
+        args.dim=50;
+        args.model = model_name.cbow;
+
+        FastText result = FastTextTrain.train(trFile, args);
+
+        System.out.println("prepare save...");
+        result.saveModel(new File("data/out/model.bin"));
+        result.saveVectors(new File("data/out/model.vec"));
+    }
+
     @Test
     public void testReadModel() throws Exception {
         FastText fastText = FastText.loadModel(new File("data/out/model.bin"));
