effeb85299977142e4afbaa257fbb430aa4e3572
jimichan <jimichan@mayabot.com>
2018-05-25 10:28:18 +0800

    1.修正配置文件加载策略，采用合并覆盖的方式

--- mynlp-pinyin/src/main/java/com/mayabot/nlp/pinyin/model/Pinyin.java
+++ mynlp-pinyin/src/main/java/com/mayabot/nlp/pinyin/model/Pinyin.java
@@ -20,7 +20,7 @@
  * <email>hankcs.cn@gmail.com</email>
  * <create-date>2014/11/2 8:05</create-date>
  *
- * <copyright file="Pinyin.java" company="上海林原信息科技有限公司">
+ * <copyright file="Pinyins.java" company="上海林原信息科技有限公司">
  * Copyright (c) 2003-2014, 上海林原信息科技有限公司. All Right Reserved, http://www.linrunsoft.com/
  * This source is subject to the LinrunSpace License. Please contact 上海林原信息科技有限公司 to get more information.
  * </copyright>
