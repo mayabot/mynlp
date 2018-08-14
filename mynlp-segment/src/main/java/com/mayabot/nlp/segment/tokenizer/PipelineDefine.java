d6b2574fc91718130b20828af128c9edafb84182
jimichan <jimichan@mayabot.com>
2018-08-14 16:57:24 +0800

    重新安排了CLI的发布方式，不通过maven仓库发布
    Former-commit-id: 18d2ac0c53dced0511ab44d9ab96b9bc28211ee0 [formerly 5f12e8a4e96d9ffe3a1ed446523c2464bc38f8c8]
    Former-commit-id: 8c479501fbc4bfbeb95de3dd351b6c39485b65f7

--- mynlp-segment/src/main/java/com/mayabot/nlp/segment/tokenizer/PipelineDefine.java
+++ mynlp-segment/src/main/java/com/mayabot/nlp/segment/tokenizer/PipelineDefine.java
@@ -6,6 +6,8 @@ import com.google.common.collect.Sets;
 import java.util.List;
 import java.util.Set;
 
+import static com.mayabot.nlp.segment.ComponentNames.*;
+
 /**
  * Pipeline的数据处理流程的定义
  *
@@ -19,6 +21,8 @@ public class PipelineDefine {
         for (Object object : strings) {
             if (object instanceof String) {
                 processorNames.add(object.toString());
+            }else if(object instanceof Enum){
+                processorNames.add(((Enum) object).name());
             } else {
                 String[] list = ((String[]) object);
                 processorNames.add(Lists.newArrayList(list));
@@ -49,18 +53,25 @@ public class PipelineDefine {
      * 内置默认的Pipeline定义
      */
     public static final PipelineDefine defaultPipeline = new PipelineDefine(
-            "customDict",
-            "mq",
-            "ml",
-            "pattern",
-            list("person", "place", "organization"),
-            "correction",
-            "speechTagging"
+            customDict,
+            mq,
+            ml,
+            pattern,
+            list(person, place, organization),
+            correction,
+            speechTagging
     );
 
 
     public static String[] list(String... ss) {
         return ss;
+    }
+    public static String[] list(Enum... ss) {
+        String[] s = new String[ss.length];
+        for (int i = 0; i < ss.length; i++) {
+            s[i] = ss[i].name();
+        }
+        return s;
     }
 
 }
