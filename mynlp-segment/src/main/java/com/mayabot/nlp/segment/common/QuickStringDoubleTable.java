dcbc27cfea8a0739f3a151a9fadb59ba1ab302c8
jimichan <jimichan@mayabot.com>
2018-08-31 18:56:31 +0800

    优化概率转移矩阵查询效率，时间缩短一半，也避免了自动拆装箱
    Former-commit-id: 606cc3e797816347d161bceb182b5baf10372d70

---
+++ mynlp-segment/src/main/java/com/mayabot/nlp/segment/common/QuickStringDoubleTable.java
@@ -0,0 +1,75 @@
+package com.mayabot.nlp.segment.common;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Table;
+
+import java.util.*;
+
+public class QuickStringDoubleTable {
+
+    private int[] labelBase;
+    private int labelSize;
+
+    double[] data;
+
+    public QuickStringDoubleTable(Table<String, String, Double> table) {
+        ArrayList<String> labelList = Lists.newArrayList(table.rowKeySet());
+
+        labelBase = findABase(labelList);
+        labelSize = labelBase.length;
+
+
+        data = new double[labelSize * labelSize];
+
+        for (String rowKey : table.rowKeySet()) {
+            for (String colKey : table.columnKeySet()) {
+                int rowid = labelBase[rowKey.hashCode() % labelSize];
+                int colid = labelBase[colKey.hashCode() % labelSize];
+
+                data[rowid * labelSize + colid] = table.get(rowKey, colKey);
+            }
+        }
+    }
+
+    public double get(String row, String col) {
+        int rowid = labelBase[row.hashCode() % labelSize];
+        int colid = labelBase[col.hashCode() % labelSize];
+        if (rowid == -1 || colid == -1) {
+            return Double.MIN_VALUE;
+        }
+        return data[rowid * labelSize + colid];
+    }
+
+    public static int[] findABase(List<String> tags) {
+
+        int len = tags.size() * 10;
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            Set<Integer> set = new HashSet<>();
+            boolean notGood = false;
+            for (String tag : tags) {
+                int code = tag.hashCode() % len;
+                if (set.contains(code)) {
+                    notGood = true;
+                    break;
+                }
+                set.add(code);
+            }
+            if (notGood) {
+                len++;
+            } else {
+                int[] base = new int[len];
+                Arrays.fill(base, -1);
+                for (int i1 = 0; i1 < tags.size(); i1++) {
+                    String tag = tags.get(i1);
+                    int index = tag.hashCode() % base.length;
+                    base[index] = i1;
+                }
+                return base;
+            }
+        }
+
+        throw new RuntimeException("findBase not find");
+    }
+
+}
