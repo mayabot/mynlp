9a98bddfb84e4b2f754c6723ac05f69e87ba63b8
jimichan <jimichan@mayabot.com>
2018-09-06 14:39:41 +0800

    cli 分词命令行工具
    Former-commit-id: 12a71c3ddce7a7f320768efc39a110f5a31716d2 [formerly b879eb5d97a6640fed8e9835dfd298ccad67f419]
    Former-commit-id: a45b275e2cf0fc595e93bc0a8121fc81354c38ee

--- mynlp-segment/src/test/java/com/mayabot/nlp/segment/TokenizerTest.java
+++ mynlp-segment/src/test/java/com/mayabot/nlp/segment/TokenizerTest.java
@@ -9,7 +9,7 @@ public class TokenizerTest {
 
     @Test
     public void test() {
-        MynlpTokenizer mynlpTokenizer = MynlpTokenizer.nlpTokenizer();
+        MynlpTokenizer mynlpTokenizer = MynlpTokenizers.coreTokenizer();
         System.out.println(mynlpTokenizer.tokenToStringList("优酷总裁魏明介绍了优酷2015年的内容战略，表示要以“大电影、大网剧、大综艺”为关键词\""));
     }
 
