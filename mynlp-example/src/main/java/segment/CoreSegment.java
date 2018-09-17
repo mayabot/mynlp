4f866b0bcb8bcb93087d7dd59218569e170a1141
jimichan <jimichan@mayabot.com>
2018-09-17 23:05:07 +0800

    独立Example项目，引入感知机子项目
    Former-commit-id: e7b0115f7714b068604ca6c5f9c11cc6fa9e5ecf

---
+++ mynlp-example/src/main/java/segment/CoreSegment.java
@@ -0,0 +1,22 @@
+package segment;
+
+import com.mayabot.nlp.segment.MynlpAnalyzer;
+import com.mayabot.nlp.segment.MynlpAnalyzers;
+import com.mayabot.nlp.segment.MynlpTokenizer;
+import com.mayabot.nlp.segment.MynlpTokenizers;
+
+import java.util.List;
+
+public class CoreSegment {
+
+    public static void main(String[] args) {
+        MynlpTokenizer mynlpTokenizer = MynlpTokenizers.coreTokenizer();
+
+        List<String> x = mynlpTokenizer.tokenToStringList("你好_lable_pos");
+
+        System.out.println(x);
+
+        MynlpAnalyzer standard = MynlpAnalyzers.standard();
+
+    }
+}
