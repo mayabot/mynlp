205040cf6e9c0a252b02205091b9100108bdb995
jimichan <jimichan@mayabot.com>
2018-09-17 23:05:07 +0800

    独立Example项目，引入感知机子项目
    Former-commit-id: 8a49d88bf74a48897caa6618f93f0a4c5ce806cb

---
+++ mynlp-example/src/main/java/segment/SimpleDictTokenizerExample.java
@@ -0,0 +1,26 @@
+package segment;
+
+import com.mayabot.nlp.segment.MynlpTokenizer;
+import com.mayabot.nlp.segment.tokenizer.SimpleDictTokenizerBuilder;
+
+public class SimpleDictTokenizerExample {
+
+    public static void main(String[] args) {
+        MynlpTokenizer tokenizer = new SimpleDictTokenizerBuilder().build();
+
+
+        String text1 = "中华人民共和国陆地面积约960万平方公里，大陆海岸线1.8万多千米，岛屿岸线1.4万多千米，内海和边海的水域面积约470多万平方千米。海域分布有大小岛屿7600多个，其中台湾岛最大，面积35798平方千米。 [1]  陆地同14国接壤，与6国海上相邻。";
+        String text = "上卷 第一回  甄士隐梦幻识通灵　贾雨村风尘怀闺秀";
+
+        tokenizer.tokenToTermList(text).forEach(
+                System.out::println
+        );
+
+        System.out.println("--------------------");
+        tokenizer = new SimpleDictTokenizerBuilder().sentenceIndexCollector().build();
+
+        tokenizer.tokenToTermList(text1).forEach(
+                System.out::println
+        );
+    }
+}
