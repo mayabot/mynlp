e1991910b4cdd7a60d2add7a191070a6120a28a1
jimichan <jimichan@mayabot.com>
2018-09-19 10:40:02 +0800

    感知机的API定义
    Former-commit-id: 514f64c60f5995e723974f93670cbefad6d3994e

---
+++ mynlp-perceptron/src/main/java/com/mayabot/nlp/perceptron/PerceptronModel.java
@@ -0,0 +1,34 @@
+package com.mayabot.nlp.perceptron;
+
+import java.io.File;
+
+/**
+ * 感知机模型
+ *
+ * @author jimichan
+ */
+public interface PerceptronModel {
+    /**
+     * 保存感知机模型实例
+     *
+     * @param file 模型文件的路径
+     */
+    void save(File file);
+
+    /**
+     * 从文件中加载模型
+     *
+     * @param file
+     */
+    void load(File file);
+
+
+    /**
+     * 对序列进行解码
+     * sequence 如果出现 -1 表示元素不存在
+     *
+     * @param sequence
+     * @return 解码后的label序列
+     */
+    int[] decode(int[] sequence);
+}
