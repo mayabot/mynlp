79439151f7cba7e360f51a7db271e18518cbb1ef
jimichan <jimichan@mayabot.com>
2018-09-19 14:36:26 +0800

    修理Test用例

---
+++ mynlp-segment/src/test/java/com/mayabot/nlp/segment/recognition/OrgTest.java
@@ -0,0 +1,36 @@
+package com.mayabot.nlp.segment.recognition;
+
+import com.mayabot.nlp.segment.MynlpTokenizer;
+import com.mayabot.nlp.segment.TokenizerTestHelp;
+import com.mayabot.nlp.segment.tokenizer.CoreTokenizerBuilder;
+import org.junit.Test;
+
+public class OrgTest {
+
+    @Test
+    public void test() {
+        {
+            String text = "这|是|上海|万行|信息|科技|有限公司|的|财务报表";
+
+            MynlpTokenizer tokenizer = new CoreTokenizerBuilder()
+                    .setOrganizationRecognition(false)
+                    .build();
+
+            TokenizerTestHelp.test(tokenizer, text);
+        }
+
+
+        {
+            String text = "这|是|上海|万行|信息科技有限公司|的|财务报表";
+
+            MynlpTokenizer tokenizer = new CoreTokenizerBuilder()
+                    .setOrganizationRecognition(true)
+                    .build();
+
+
+            TokenizerTestHelp.test(tokenizer, text);
+        }
+
+
+    }
+}
