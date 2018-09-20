5e5147b08b6ff564462f328a2e1c97e110ec4b35
jimichan <jimichan@mayabot.com>
2018-09-20 13:15:48 +0800

    增加Summary模块，暂时从Hanlp中重构而来，后续继续优化
    Former-commit-id: f36d1c4010ed4e096998adf11476592966e71991

--- mynlp-segment/src/main/java/com/mayabot/nlp/segment/tokenizer/collector/SentenceCollector.java
+++ mynlp-segment/src/main/java/com/mayabot/nlp/segment/tokenizer/collector/SentenceCollector.java
@@ -1,6 +1,5 @@
 package com.mayabot.nlp.segment.tokenizer.collector;
 
-import com.google.common.collect.Lists;
 import com.mayabot.nlp.segment.WordTerm;
 import com.mayabot.nlp.segment.WordTermCollector;
 import com.mayabot.nlp.segment.wordnet.Vertex;
@@ -9,7 +8,6 @@ import com.mayabot.nlp.segment.wordnet.Wordpath;
 import com.mayabot.nlp.utils.StringUtils;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.function.Consumer;
 
 /**
@@ -21,14 +19,13 @@ public class SentenceCollector implements WordTermCollector {
 
     @Override
     public void collect(Wordnet wordnet, Wordpath wordPath, Consumer<WordTerm> consumer) {
+
         Iterator<Vertex> vertexIterator = wordPath.iteratorVertex();
+
         while (vertexIterator.hasNext()) {
             Vertex vertex = vertexIterator.next();
+            WordTerm term = new WordTerm(vertex.realWord(), vertex.guessNature(), vertex.getRowNum());
 
-            WordTerm term = new WordTerm(vertex.realWord(), vertex.guessNature());
-            term.setOffset(vertex.getRowNum());
-
-            //默认把空白的字符去除掉
             if (StringUtils.isWhiteSpace(term.word)) {
                 continue;
             }
@@ -37,21 +34,4 @@ public class SentenceCollector implements WordTermCollector {
         }
     }
 
-    public static void main(String[] args) {
-
-        List<B> listB = Lists.newArrayList();
-        List<? extends A> listA = listB;
-
-        System.out.println(listA);
-
-    }
-
-
-    static class A {
-
-    }
-
-    static class B extends A {
-
-    }
 }
