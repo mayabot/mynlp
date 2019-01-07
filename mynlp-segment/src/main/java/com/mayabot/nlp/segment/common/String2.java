4fae062e7c0c8f3153e054cd2f43adc7347cf694
jimichan <jimichan@mayabot.com>
2019-01-07 13:20:33 +0800

    删除两个无效类
    Former-commit-id: ee5aa4ebf8bf6e4ba8d0dbf403ecee90cd8bf2f7

---
+++ mynlp-segment/src/main/java/com/mayabot/nlp/segment/common/String2.java
@@ -0,0 +1,61 @@
+package com.mayabot.nlp.segment.common;
+
+import org.jetbrains.annotations.NotNull;
+
+public class String2 implements CharSequence {
+
+
+    @NotNull
+    private char[] chars;
+
+    private int start = 0;
+    private int end = 0;
+
+    private int len = 0;
+
+
+    public String2(@NotNull char[] chars) {
+        this.chars = chars;
+        start = 0;
+        this.end = chars.length;
+        len = chars.length;
+    }
+
+    public String2(@NotNull char[] chars, int start, int end) {
+        this.chars = chars;
+        this.start = start;
+        this.end = end;
+        this.len = end - start;
+    }
+
+    public void setStartEnd(int start, int end) {
+        this.start = start;
+        this.end = end;
+        this.len = end - start;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    @Override
+    public int length() {
+        return len;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return chars[index + start];
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return new String2(chars, this.start + start, this.start + end);
+    }
+
+    @Override
+    public String toString() {
+        return new String(chars, start, len);
+    }
+
+}
