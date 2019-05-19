c1da82e50d417b923f6a14b8db49dc8681f3c314
jimichan <jimichan@mayabot.com>
2019-05-19 18:13:33 +0800

    fix smart atom offset bug
    Former-commit-id: ed63ebb866a1db22ad10d4bfe851e6525a7432ca

--- mynlp-segment/src/test/java/com/mayabot/nlp/segment/OffsetTest.kt
+++ mynlp-segment/src/test/java/com/mayabot/nlp/segment/OffsetTest.kt
@@ -43,6 +43,15 @@ class OffsetTest {
     }
 
     @Test
+    fun testE1(){
+        val text = "俞正声主持召开全国政协第五十三次主席会议"
+        val lexer = Lexers.coreBuilder().withPersonName().build()
+        lexer.scan(text).toList().forEach { w->
+            println("${w.word} ${w.offset} ${w.length()}")
+        }
+    }
+
+    @Test
     fun test22() {
         val wordTerms = Lexers.core().filterReader(true, true).scan(StringReader(text))
 
