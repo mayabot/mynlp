5958180a577afd03c4e1af45592b92ed1192048a
jimichan <jimichan@mayabot.com>
2019-06-26 23:18:27 +0800

    Perceptron代码整理

--- mynlp-perceptron/src/main/java/com/mayabot/nlp/perceptron/PerceptronDefinition.kt
+++ mynlp-perceptron/src/main/java/com/mayabot/nlp/perceptron/PerceptronDefinition.kt
@@ -146,15 +146,15 @@ abstract class PerceptronDefinition<E, Label, InputSequence> {
         println("构建FeatureSet耗时 ${System.currentTimeMillis() - t1} MS, 包含${featureSet.size()}个特征")
 
         //计算有多少行
-        var lineCount_ = 0
+        var lineCountLocal = 0
         trainFiles.forEach { file ->
             file.forEachLine { line ->
                 if (line.isNotBlank()) {
-                    lineCount_++
+                    lineCountLocal++
                 }
             }
         }
-        val lineCount = lineCount_
+        val lineCount = lineCountLocal
 
         val sampleList = ArrayList<TrainSample>(lineCount)
         // 加载样例
@@ -299,10 +299,10 @@ data class EvaluateResult(
     /**
      * F1综合指标
      */
-    val F1: Float
+    val f1: Float
         get() = (2.0 * precision * recall / (precision + recall)).toFloat()
 
     override fun toString(): String {
-        return "正确率(P) %.2f , 召回率(R) %.2f , F1 %.2f".format(precision, recall, F1)
+        return "正确率(P) %.2f , 召回率(R) %.2f , F1 %.2f".format(precision, recall, f1)
     }
 }
