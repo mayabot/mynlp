64400f33700fbc9bde87c143b8d0b6642dfecbe9
jimichan <jimichan@mayabot.com>
2019-06-26 23:20:30 +0800

    Perceptron代码整理，增加copyright
    Former-commit-id: 0c5efa3b4012e32fbd55bf93b0f11909549ad84c

--- mynlp-perceptron/src/main/java/com/mayabot/nlp/perceptron/EvaluateUtils.kt
+++ mynlp-perceptron/src/main/java/com/mayabot/nlp/perceptron/EvaluateUtils.kt
@@ -1,26 +1,41 @@
+/*
+ * Copyright 2018 mayabot.com authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mayabot.nlp.perceptron
 
 
 /**
  * 简单评估。只考虑标签相等性
  */
-fun simpleEvaluate(model: Perceptron, samples: List<TrainSample>) : EvaluateResult{
+fun simpleEvaluate(model: Perceptron, samples: List<TrainSample>): EvaluateResult {
 
     var total = 0
     var right = 0
 
-    samples.forEach { sample->
+    samples.forEach { sample ->
         total += sample.label.size
         val gold = sample.label
         val pred = model.decode(sample.featureSequence)
         for (i in 0 until sample.label.size) {
-            if(gold[i] == pred[i]){
+            if (gold[i] == pred[i]) {
                 right++
             }
         }
     }
 
-    return EvaluateResult(total,total,right)
+    return EvaluateResult(total, total, right)
 }
 
 /**
