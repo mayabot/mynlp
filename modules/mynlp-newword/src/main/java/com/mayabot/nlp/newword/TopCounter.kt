f65af2e39ffe06128706ed013577f3b00f2c86ad
jimichan <jimichan@mayabot.com>
2019-09-16 16:29:24 +0800

    test new word
    Former-commit-id: b1637a0cac65b6880d6ecd89358c14015254646a

--- modules/mynlp-newword/src/main/java/com/mayabot/nlp/newword/TopCounter.kt
+++ modules/mynlp-newword/src/main/java/com/mayabot/nlp/newword/TopCounter.kt
@@ -1,41 +1,47 @@
 package com.mayabot.nlp.newword
 
-import com.carrotsearch.hppc.ObjectIntHashMap
-
 /**
  * @author jimichan
  * 统计元素重复最高
  */
-class TopCounter(val size: Int = 2000000, var minCount: Int = 10) {
+class TopCounter(private val size: Int = 2000000,
+                 private var minCount: Int = 10) {
 
-    var verbose = false
+    private var verbose = false
 
-    var topedMap = ObjectIntHashMap<String>(size)
+    var data = HashMap<String, IntCount>(size)
 
-    var topList = listOf<String>()
+    private var topList = listOf<String>()
 
-
-    var lastMinCount = 2
+    private var lastMinCount = 2
 
     fun put(key: String) {
-        topedMap.putOrAdd(key, 1, 1)
 
-        if (topedMap.size() >= size) {
+        val v = data[key]
+        if (v == null) {
+            data[key] = IntCount()
+        } else {
+            v.value++
+        }
+
+        if (data.size >= size) {
             reduce()
         }
     }
 
     private fun reduce() {
         //1. remove count less min
-        if (verbose) println("清洗前有${topedMap.size()}条数据")
+        if (verbose) println("清洗前有${data.size}条数据")
+
         val target = size / 4 //压缩为1/4
 
         var max = 0
 
         for (min in lastMinCount until minCount) {
-            if (topedMap.size() > target) {
-                topedMap.removeAll { _, value -> value <= min }
-                if (verbose) println("删除小于 ${min} 的数量,剩余${topedMap.size()}")
+            if (data.size > target) {
+                //data.removeAll { _, value -> value <= min }
+                data = data.filterTo(HashMap()) { it.value.value > min }
+                if (verbose) println("删除小于 ${min} 的数量,剩余${data.size}")
                 max = min
             }
         }
@@ -46,7 +52,7 @@ class TopCounter(val size: Int = 2000000, var minCount: Int = 10) {
         }
 
         //还超出一半
-        if (topedMap.size() > size / 2) {
+        if (data.size > size / 2) {
             minCount++
         }
 
@@ -54,18 +60,20 @@ class TopCounter(val size: Int = 2000000, var minCount: Int = 10) {
     }
 
     fun clean() {
-        topedMap.removeAll { _, value -> value <= minCount }
+        data = data.filterTo(HashMap()) { it.value.value > minCount }
     }
-
 
     fun getListResult(): List<WordCount> {
         clean()
-        val list = ArrayList<WordCount>(topedMap.size())
-        topedMap.forEach { cusor ->
-            list += WordCount(cusor.key!!, cusor.value)
+        val list = ArrayList<WordCount>(data.size)
+
+        data.forEach {
+            list += WordCount(it.key, it.value.value)
         }
+
         list.sort()
         return list
     }
 
 }
+
---
+++ modules/mynlp-newword/src/main/java/com/mayabot/nlp/newword/ValueObjects.kt
@@ -0,0 +1,113 @@
+package com.mayabot.nlp.newword
+
+import com.carrotsearch.hppc.CharIntScatterMap
+import com.carrotsearch.hppc.IntHashSet
+import kotlin.math.*
+
+class IntCount {
+    var value : Int = 1
+}
+
+/**
+ * 词和词数量
+ */
+class WordCount(val word: String, val count: Int) : Comparable<WordCount> {
+    override fun compareTo(other: WordCount): Int {
+        return other.count.compareTo(count)
+    }
+}
+
+data class NewWord(
+        val word: String,
+        val len: Int,
+        val freq: Int,
+        val docFreq: Int,
+        val mi: Float,
+        val avg_mi: Float,
+        val entropy: Float,
+        val le: Float,
+        val re: Float,
+        val idf: Float,
+        val isBlock: Boolean
+) {
+    var score: Float = 0f
+
+    /**
+     * 内置打分公式
+     */
+    fun doScore() {
+        var ac = abs(le -re)
+        if (ac == 0.0f) {
+            ac = 0.00000000001f
+        }
+        score = avg_mi + log2( (le * exp(re) + re * exp(le))/ac)
+//        score = avg_mi + entropy + idf * 1.5f + len * freq / docFreq
+        if (isBlock) score += 1000
+    }
+}
+
+class WordInfo(val word: String) {
+
+    companion object {
+        val empty = CharIntScatterMap(10)
+        val emptySet = IntHashSet(10)
+    }
+
+    var count = 0
+    var mi = 0f
+
+    var mi_avg = 0f
+
+    // 是否被双引号 书名号包围
+    var isBlock = false
+
+    var entropy = 0f
+
+    var left = CharIntScatterMap(10)
+    var right = CharIntScatterMap(10)
+
+    var docSet = IntHashSet()
+
+    var score = 0f
+
+    var idf = 0f
+
+    var doc = 0
+
+    var le = 0f
+    var re = 0f
+
+//    var tfIdf =0f
+
+    fun tfIdf(docCount: Double, ziCount: Double) {
+        val doc = docSet.size() + 1
+        idf = log10(docCount / doc).toFloat()
+//        tfIdf = (idf * (count/ziCount)).toFloat()
+        docSet = emptySet
+        this.doc = doc - 1
+    }
+
+    fun entropy() {
+
+        var leftEntropy = 0f
+        for (entry in left) {
+            val p = entry.value / count.toFloat()
+            leftEntropy -= (p * ln(p.toDouble())).toFloat()
+        }
+
+        var rightEntropy = 0f
+        for (entry in right) {
+            val p = entry.value / count.toFloat()
+            rightEntropy -= (p * ln(p.toDouble())).toFloat()
+        }
+
+        le = leftEntropy
+        re = rightEntropy
+
+        entropy = min(leftEntropy, rightEntropy)
+
+        left = empty
+        right = empty
+    }
+
+}
