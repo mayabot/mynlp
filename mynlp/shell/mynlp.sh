c6e6a19847e6f63f43465f392a102f8cfe946efc
jimichan <jimichan@mayabot.com>
2020-12-23 11:06:30 +0800

    mynlp cli shell

---
+++ mynlp/shell/mynlp.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+# Attempt to set APP_HOME
+# Resolve links: $0 may be a link
+PRG="$0"
+# Need this for relative symlinks.
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+SAVED="`pwd`"
+cd "`dirname \"$PRG\"`/" >/dev/null
+APP_HOME="`pwd -P`"
+cd "$SAVED" >/dev/null
+
+APP_NAME="Mynlp"
+APP_BASE_NAME=`basename "$0"`
+
+# 下载mynlp-bin.jar
+# 准备JDK环境变量
+# exe jar
